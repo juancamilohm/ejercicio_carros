b50a82bffe96116972dc5e87f1b7f3df6172cfdf
juanc <juan.1701410447@ucaldas.edu.co>
2021-07-23 21:53:30 -0500

    Commit 3

--- src/Carro.java
+++ src/Carro.java
@@ -1,16 +1,23 @@
+
+/**
+ * Clase que permite instanciar un carro
+ * @author Oscar Buriticá
+ * @author Juan C Henao M
+ * @version 2.0
+ */
 public class Carro {
     String placa;
     double galones;
     int modelo;
 
     /**
-     * 
+     * Constructor
      */
     public Carro() {
     }
 
     /**
-     * 
+     * Constructor que permite modelar un carro.
      * @param placa
      * @param galones
      * @param modelo
--- src/DatosCarros.java
+++ src/DatosCarros.java
@@ -1,13 +1,27 @@
 import java.util.ArrayList;
 
+/**
+ * Clase que permite instanciar los datos de un carro.
+ * @author Oscar Buriticá
+ * @author Juan C Henao M
+ * @version 2.0
+ */
 public class DatosCarros {
     
+    private ArrayList<Carro>carros; 
+
+    
+    public DatosCarros() {
+        carros = new ArrayList<>();
+    }
+
     /**
-     * 
-     * @param carro
+     * permite guardar un carro en la lista de carros.
+     * @param carro carro que se registra
      */
     public void registrarCarro(Carro carro){
-        
+        carros.add(carro);
+
     }
 
     /**
